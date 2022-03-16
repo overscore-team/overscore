94b92fd64165b2dc6add036569acde929abe71f0
cinyearchan <cinyearchan@gmail.com>
2022-03-17 01:09:14 +0800

    feat: init configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+	"root": true,
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": [
+    "standard",
+		"eslint:recommended",
+		"plugin:prettier/recommended"
+  ],
+  "parserOptions": {
+		"parser": "@babel/eslint-parser",
+    "ecmaVersion": "latest"
+  },
+	rules: {
+		"prettier/prettier": "error"
+	}
+}
