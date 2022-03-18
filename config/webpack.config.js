858616f55a1bea6eb7b7e4fbd685859382d3e0db
cinyearchan <cinyearchan@gmail.com>
2022-03-19 01:49:52 +0800

    feat: 更新基础配置

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     filename: `${pkg.name}.min.js`,
     path: path.resolve(__dirname, '../dist'),
     library: `${pkg.name}`,
-    libraryTarget: 'umd',
+    libraryTarget: 'umd'
   },
   module: {
     rules: [
